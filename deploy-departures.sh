50eb21dc73c0b3226a963350bc4b0461a77b06a2
Serge Voilokov <serge0x76@gmail.com>
2014-12-24 14:40:08 -0500

    info and test actions added to script

--- deploy-departures.sh
+++ deploy-departures.sh
@@ -1,11 +1,27 @@
 host=`cat ~/.config/departures/host.txt`
 
-if [ x$1 == "x-i" ]; then
-	ssh $host "sudo cp ~/src/xtree/ctests/departures /usr/local/bin/; /usr/local/bin/departures -v;"
-else
+if [ x$1 == "x-l" ]; then
+	ssh -t $host "
+		echo == executing install on host: \$HOSTNAME ==
+		sudo cp ~/src/xtree/ctests/departures /usr/local/bin/
+		/usr/local/bin/departures -v"
+elif [ x$1 == "x-i" ]; then
+	ssh -t $host "
+		echo == executing info on host: \$HOSTNAME ==
+		crontab -l|grep departures
+		/usr/local/bin/departures -v"
+elif [ x$1 == "x-t" ]; then
+	ssh -t $host "
+		echo == executing test on host: \$HOSTNAME ==
+		crontab -l|grep departures
+		/usr/local/bin/departures -v
+		/usr/local/bin/departures -f XG -m"
+elif [ x$1 == "x-b" ]; then
+	echo == executing buld on host: \$HOSTNAME ==
 	ssh $host "
 		cd ~/src/xtree/ctests
 		git pull
+		make clean
 		make
 		echo == new version ==
 		./departures -v
@@ -16,5 +32,11 @@ else
 
 	echo
 	echo Project was build on $host. For install run script with -i parameter.
+else
+	echo usage: deploy-departures.h [-bitl]
+	echo '    ' -i       info
+	echo '    ' -b       build
+	echo '    ' -l       install
+	echo '    ' -t       test
 fi
 
