11e8ed94638551c7b423490803f39a391a815d94
Serge Voilokov <serge0x76@gmail.com>
2015-09-01 22:51:14 -0400

    Move common and weather to the separate repositories.

--- weather/macports.cmake
+++
@@ -1,4 +0,0 @@
-# I keep all development libs in /opt/ports/dev
-# So use: cmake -DCMAKE_TOOLCHAIN_FILE=~/src/xtree/ctests/weather/macports.cmake ~/src/xtree/ctests/weather
-
-set(CMAKE_FIND_ROOT_PATH /opt/ports/dev)
